1446a048898b1b12385bce823cdf02164db2dea7
Dan Vega <danvega@gmail.com>
2012-10-19 21:42:04 -0400

    Initial Commit

---
+++ slide_config.js
@@ -0,0 +1,36 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Responsive Web Apps<br>Bootstrap 2.0',
+    subtitle: 'WebSIG Cleveland 10/20/12',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/bootstrap_24.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Dan Vega',
+    company: 'Senior Developer, FirstComp',
+    gplus: 'http://plus.google.com/100658587973307989038',
+    twitter: '@cfaddict',
+    www: 'http://www.danvega.org',
+    github: 'http://github.com/cfaddict'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
